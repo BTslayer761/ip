28f82471696aab4643ee874b8624d467d23c69a3
BTslayer761 <brendantey761@gmail.com>
2025-08-20 20:02:42 +0800

    Change the text a greeting and goodbye

---
+++ src/main/java/Bambot.java
@@ -0,0 +1,10 @@
+public class Bambot {
+    public static void main(String[] args) {
+        System.out.println("_________________________");
+        System.out.println("Hello! I'm Bambot");
+        System.out.println("What can I do for you?");
+        System.out.println("__________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("__________________________");
+    }
+}
