c90d4a72cbf244a24960e6ae05e7e830fb42c090
BTslayer761 <brendantey761@gmail.com>
2025-08-26 16:18:08 +0800

    Updated echo function

--- src/main/java/Bambot.java
+++ src/main/java/Bambot.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
 
 public class Bambot {
-    static Scanner messageInput = new Scanner(System.in);
-    private static void echo(String message){
-        System.out.println("_________________________");
-        System.out.println(message);
-        System.out.println("__________________________");
+    static Scanner scanner = new Scanner(System.in);
+    private static void echo(){
+        while(true) {
+            String message = scanner.nextLine();
+            if(message.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("_________________________");
+            System.out.println(message);
+            System.out.println("__________________________");
+        }
     }
 
     public static void main(String[] args) {
@@ -14,16 +20,8 @@ public class Bambot {
         System.out.println("What can I do for you?");
         System.out.println("__________________________");
 
-        String message = "";
-        while(true) {
-            message = messageInput.nextLine();
-            if(message.equalsIgnoreCase("bye")){
-                break;
-            }
-            else{
-                echo(message);
-            }
-        }
+        echo();
+
         System.out.println("__________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("__________________________");
