06de88341a873c6544752c7b5f64c54a9eea45a6
BTslayer761 <brendantey761@gmail.com>
2025-09-26 15:07:29 +0800

    resolve issue of marked tasked being unmarked when written back into the Array

---
+++ src/main/java/Bambot/tasks/Task.java
@@ -0,0 +1,41 @@
+package Bambot.tasks;
+
+public class Task {
+    protected final String Description;
+    protected Boolean isDone;
+
+    public Task(String task, boolean isDone) {
+        Description = task;
+        this.isDone = isDone;
+    }
+
+    public boolean getIsDone() {
+        return isDone;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+
+    public void markTask() {
+        isDone = true;
+    }
+
+    public void unmarkTask() {
+        isDone = false;
+    }
+
+    public String toString() {
+        if (isDone) {
+            return "[x]" + Description;
+        } else {
+            return "[ ]" + Description;
+        }
+    }
+
+    public String toStorageString() {
+        return (this.getClass().getSimpleName() + "," + Description + "," + isDone);
+    }
+
+}
