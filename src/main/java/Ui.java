542ca201b65c6f5e0e8afd2ffdf8a1d3c19deef4
BTslayer761 <brendantey761@gmail.com>
2025-09-30 12:04:36 +0800

    Updated printCommand()

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -21,15 +21,16 @@ public class Ui {
     public static void printCommands() {
         System.out.println(DIVIDER);
         System.out.println("Here are the available commands:");
-        System.out.println("  list                              - Show all tasks");
-        System.out.println("  todo <description>                - Add a ToDo task");
-        System.out.println("  deadline <description> /by <date> - Add a Deadline task");
-        System.out.println("  event <description> /at <time>    - Add an Event task");
-        System.out.println("  done <task number>                - Mark a task as done");
-        System.out.println("  delete <task number>              - Delete a task");
-        System.out.println("  find <keyword>                    - Find tasks containing the keyword");
-        System.out.println("  help                              - Show this list of commands");
-        System.out.println("  bye                               - Exit the program");
+        System.out.println("  list                                          - Show all tasks");
+        System.out.println("  todo <description>                            - Add a ToDo task");
+        System.out.println("  deadline <description> /by <date>             - Add a Deadline task");
+        System.out.println("  event <description> /from <time> /to <time>   - Add an Event task");
+        System.out.println("  mark <task number>                            - Mark a task");
+        System.out.println("  unmark <task number>                          - Unmark a task");
+        System.out.println("  delete <task number>                          - Delete a task");
+        System.out.println("  find <keyword>                                - Find tasks containing the keyword");
+        System.out.println("  help                                          - Show this list of commands");
+        System.out.println("  bye                                           - Exit the program");
         System.out.println(DIVIDER);
     }
 }
