1c4500f13915f70c0eee8e4e4da5d05786755a46
BTslayer761 <brendantey761@gmail.com>
2025-09-30 20:58:49 +0800

    Add line divider when printing errors

--- src/main/java/BambotException.java
+++ src/main/java/BambotException.java
@@ -1,6 +1,6 @@
 public class BambotException extends Exception {
     public BambotException(String message) {
 
-        super(message);
+        super(Ui.DIVIDER + "\n" + message + "\n" + Ui.DIVIDER);
     }
 }
